cf180e21bc10b785e62b817ab2bf16c2e931ff8a
Sekul <wojowniksek@gmail.com>
2024-07-11 01:20:18 +0200

    init repo using nextra docs template

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+    logo: <span>My Project</span>,
+    project: {
+        link: "https://github.com/VZN-Scripts/docs",
+    },
+    chat: {
+        link: "https://discord.gg/wqzrVM3QYk",
+    },
+    docsRepositoryBase: "https://github.com/VZN-Scripts/docs/blob/main",
+    footer: {
+        text: "VZN Scripts",
+    },
+};
+
+export default config;
