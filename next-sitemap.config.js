9313e33a27ffc84ee6b4b5cb91e02404200f4a9f
Sekul <wojowniksek@gmail.com>
2024-07-30 02:34:38 +0200

    feat: sitemap building

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: "https://docs.vzn-scripts.com/",
+    generateRobotsTxt: true,
+    generateIndexSitemap: false,
+    outDir: "./out",
+};
