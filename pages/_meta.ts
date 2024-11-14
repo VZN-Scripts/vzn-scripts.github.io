701649d983d1a40406cd0d768243cd5c5e8db58a
Sekul <wojowniksek@gmail.com>
2024-11-14 01:00:34 +0100

    feat(vzn-references): finish docs to this script

--- pages/_meta.ts
+++ pages/_meta.ts
@@ -2,4 +2,5 @@ export default {
     index: "Introduction",
     "vzn-notify": "VZN Notify",
     "vzn-cleanerjob": "VZN Cleanerjob",
+    "vzn-references": "VZN References",
 };
---
+++ pages/vzn-references/_meta.ts
@@ -0,0 +1,5 @@
+export default {
+    config: "Configuration",
+    framework: "Framework",
+    custom_reward: "Custom Reward",
+};
