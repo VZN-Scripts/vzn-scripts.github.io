dc53165c14b490227a85be21e87abf7818a4ef5e
Sekul <wojowniksek@gmail.com>
2024-11-18 18:51:11 +0100

    feat: add simple separator in navbar

--- pages/_meta.ts
+++ pages/_meta.ts
@@ -1,5 +1,8 @@
 export default {
     index: "Introduction",
+    "--": {
+        type: "separator",
+    },
     "vzn-notify": "VZN Notify",
     "vzn-cleanerjob": "VZN Cleanerjob",
     "vzn-references": "VZN References",
