178d2755627a8a35903b241bda52d0eba01a1bec
Sekul <wojowniksek@gmail.com>
2024-11-19 17:52:32 +0100

    tweak: default collapse level in sidebar

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { DocsThemeConfig, useConfig } from "nextra-theme-docs";
 import { useRouter } from "next/router";
-import { GoogleAnalytics } from '@next/third-parties/google'
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const config: DocsThemeConfig = {
     head: () => {
@@ -10,10 +10,9 @@ const config: DocsThemeConfig = {
         const defaultDescription = "Documentation for VZN Scripts assets";
         const url = `https://docs.vzn-scripts.com/${asPath}`;
 
-        const googleId = "G-XV58K6R2PX"
+        const googleId = "G-XV58K6R2PX";
 
-        const fullTitle = `${title} | VZN Scripts Docs`
-
+        const fullTitle = `${title} | VZN Scripts Docs`;
 
         return (
             <>
@@ -22,23 +21,11 @@ const config: DocsThemeConfig = {
                 <link rel="icon" type="image/png" href="/logo.png" />
 
                 <meta httpEquiv="Content-Language" content="en" />
-                <meta
-                    name="description"
-                    content={frontMatter.description || defaultDescription}
-                />
-                <meta
-                    name="og:title"
-                    content={fullTitle || "VZN Scripts Documentation"}
-                />
+                <meta name="description" content={frontMatter.description || defaultDescription} />
+                <meta name="og:title" content={fullTitle || "VZN Scripts Documentation"} />
                 <meta name="og:url" content={url} />
-                <meta
-                    name="og:description"
-                    content={frontMatter.description || defaultDescription}
-                />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1.0"
-                />
+                <meta name="og:description" content={frontMatter.description || defaultDescription} />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             </>
         );
     },
@@ -54,8 +41,7 @@ const config: DocsThemeConfig = {
                 style={{
                     width: "38px",
                     height: "38px",
-                    background:
-                        "url('https://avatars.githubusercontent.com/u/175264237?s=38') no-repeat left",
+                    background: "url('https://avatars.githubusercontent.com/u/175264237?s=38') no-repeat left",
                     backgroundSize: "38px",
                     borderRadius: "50%",
                 }}
@@ -75,19 +61,21 @@ const config: DocsThemeConfig = {
     chat: {
         link: "https://discord.gg/wqzrVM3QYk",
     },
-    docsRepositoryBase:
-        "https://github.com/VZN-Scripts/vzn-scripts.github.io/blob/main",
+    docsRepositoryBase: "https://github.com/VZN-Scripts/vzn-scripts.github.io/blob/main",
     footer: {
         content: <>VZN-Scripts</>,
     },
     color: {
         hue: 203,
-        saturation: 100
+        saturation: 100,
+    },
+    sidebar: {
+        defaultMenuCollapseLevel: 1,
     },
     toc: {
         backToTop: true,
     },
-    navigation: true
+    navigation: true,
 };
 
 export default config;
