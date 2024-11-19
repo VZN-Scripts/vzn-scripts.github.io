17708848a22725e8a94e6e19d782487c748946bd
Sekul <wojowniksek@gmail.com>
2024-11-19 17:52:10 +0100

    style: prettier code

--- .prettierrc.js
+++ .prettierrc.js
@@ -5,16 +5,7 @@ const config = {
     printWidth: 120,
     singleQuote: false,
     trailingComma: "all",
-    /**
-     * Note:
-     * The order of plugins in this array is crucial for functionality.
-     * Do not change the order, as this will prevent both plugins from working correctly.
-     */
-    plugins: ["@trivago/prettier-plugin-sort-imports", "prettier-plugin-tailwindcss"],
-    importOrder: ["^node:", "<THIRD_PARTY_MODULES>", "^[./]"],
-    importOrderParserPlugins: ["typescript", "classProperties", "decorators-legacy"],
-    importOrderSeparation: true,
-    importOrderSortSpecifiers: true,
+    plugins: ["prettier-plugin-tailwindcss"],
     tailwindFunctions: ["cn"],
 };
 
